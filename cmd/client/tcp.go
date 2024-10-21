363bd993519df9b6d8cb9b096b84ef6ca9589ba9
Augustin Braslasu <b.augustin@gmail.com>
2024-10-21 20:15:12 +0300

    implement retry

--- cmd/client/tcp.go
+++ cmd/client/tcp.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log/slog"
 	"net"
+	"time"
 
 	"k8s.io/apimachinery/pkg/util/httpstream"
 
@@ -13,9 +14,26 @@ import (
 	"github.com/knight42/krelay/pkg/xnet"
 )
 
+const maxRetries = 3
+const retryDelay = 2 * time.Second
+
 func handleTCPConn(clientConn net.Conn, serverConn httpstream.Connection, dstAddr xnet.Addr, dstPort uint16) {
 	defer clientConn.Close()
+	l := slog.With(slog.String(constants.LogFieldRequestID, xnet.NewRequestID()))
 
+	var attempt int
+	for attempt = 0; attempt < maxRetries; attempt++ {
+		l.Info("Attempt:")
+		if handleConnection(clientConn, serverConn, dstAddr, dstPort) {
+			return
+		}
+		time.Sleep(retryDelay)
+	}
+
+	l.Error("Max retries reached, giving up")
+}
+
+func handleConnection(clientConn net.Conn, serverConn httpstream.Connection, dstAddr xnet.Addr, dstPort uint16) bool {
 	requestID := xnet.NewRequestID()
 	l := slog.With(slog.String(constants.LogFieldRequestID, requestID))
 	defer l.Debug("handleTCPConn exit")
@@ -28,7 +46,7 @@ func handleTCPConn(clientConn net.Conn, serverConn httpstream.Connection, dstAdd
 	dataStream, errorChan, err := createStream(serverConn, requestID)
 	if err != nil {
 		l.Error("Fail to create stream", slogutil.Error(err))
-		return
+		return false
 	}
 
 	hdr := xnet.Header{
@@ -40,54 +58,49 @@ func handleTCPConn(clientConn net.Conn, serverConn httpstream.Connection, dstAdd
 	_, err = xio.WriteFull(dataStream, hdr.Marshal())
 	if err != nil {
 		l.Error("Fail to write header", slogutil.Error(err))
-		return
+		return false
 	}
 
 	var ack xnet.Acknowledgement
 	err = ack.FromReader(dataStream)
 	if err != nil {
 		l.Error("Fail to receive ack", slogutil.Error(err))
-		return
+		return false
 	}
 	if ack.Code != xnet.AckCodeOK {
 		l.Error("Fail to connect", slogutil.Error(ack.Code))
-		return
+		return false
 	}
 
 	localError := make(chan struct{})
 	remoteDone := make(chan struct{})
 
 	go func() {
-		// Copy from the remote side to the local port.
 		if _, err := io.Copy(clientConn, dataStream); err != nil && !xnet.IsClosedConnectionError(err) {
 			l.Error("Fail to copy from remote stream to local connection", slogutil.Error(err))
 		}
-
-		// inform the select below that the remote copy is done
 		close(remoteDone)
 	}()
 
 	go func() {
-		// inform server we're not sending any more data after copy unblocks
 		defer dataStream.Close()
-
-		// Copy from the local port to the remote side.
 		if _, err := io.Copy(dataStream, clientConn); err != nil && !xnet.IsClosedConnectionError(err) {
 			l.Error("Fail to copy from local connection to remote stream", slogutil.Error(err))
-			// break out of the select below without waiting for the other copy to finish
 			close(localError)
 		}
 	}()
 
-	// wait for either a local->remote error or for copying from remote->local to finish
 	select {
 	case <-remoteDone:
 	case <-localError:
+		return false
 	}
 
-	// always expect something on errorChan (it may be nil)
 	err = <-errorChan
 	if err != nil {
 		l.Error("Unexpected error from stream", slogutil.Error(err))
+		return false
 	}
+
+	return true
 }
